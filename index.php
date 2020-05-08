d04dd1a6ccb555ef0ccf2f676f43a26bcc6e0fea
Klaus Pecina <pecina222@gmail.com>
2020-05-08 17:25:56 +0200

    1.1.0

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'vendor/autoload.php';
+use Doctrine\DBAL\DriverManager;
+
+$connectionParams = array(
+    'dbname' => 'golfbank',
+    'user' => 'root',
+    'password' => '',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+);
+$conn = DriverManager::getConnection($connectionParams);
+
+$queryBuilder = $conn->createQueryBuilder();
+
+$queryBuilder
+    ->select("name","parscore")
+    ->from("course");
+
+$stmt = $conn->query($queryBuilder);
+
+
+$courses = $stmt -> fetchAll();
+
+$view = new \TYPO3Fluid\Fluid\View\TemplateView();
+
+$paths = $view ->getTemplatePaths();
+$paths -> setTemplatePathAndFilename(__DIR__ . "/courses.html");
+$values["courses"] = $courses;
+
+$view -> assignMultiple($values);
+
+$output = $view-> render();
+
+echo $output;
